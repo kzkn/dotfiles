438dc0e3d5dafa9c0c21bcab744d8bfdfc653e04
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2018-03-19 22:41:23 +0900

    使わなくなったコマンドを削除

--- emacs/defuns.el
+++ emacs/defuns.el
@@ -1,49 +1,4 @@
-(eval-when-compile
-  (require 'cl))
-
-(defun google ()
-  (interactive)
-  (browse-url
-   (concat
-    "http://www.google.com/search?ie=utf-8&oe=utf-8&q="
-    (if (region-active-p)
-        (buffer-substring (region-beginning) (region-end))
-      (read-string "Query: ")))))
-
-(defun buffer-current-line-string ()
-  (save-excursion
-    (buffer-substring (progn (beginning-of-line) (point))
-                      (progn (end-of-line) (point)))))
-
-(defun buffer-current-line-starts-with (prefix)
-  (let ((line (buffer-current-line-string)))
-    (and (<= (length prefix) (length line))
-         (equal prefix (substring line 0 (length prefix))))))
-
-(defun open-path ()
-  (interactive)
-  (save-excursion
-    (let ((path (if (%thunar-path-p)
-                    (buffer-current-line-string)
-                  (%win-to-thunar))))
-      (start-process "open-path" nil "/usr/bin/thunar" path))))
-
-(defun %thunar-path-p ()
-  (buffer-current-line-starts-with "smb:"))
-
-(defun %win-to-thunar ()
-  (let ((orig-path (buffer-current-line-string)))
-    (with-temp-buffer
-      (insert orig-path)
-      (%replace-path-separators)
-      (buffer-string))))
-
-(defun %replace-path-separators ()
-  (let ((end (progn (end-of-line) (point))))
-    (beginning-of-line)
-    (insert "smb:")
-    (while (and (search-forward "\\" nil t) (<= (point) end))
-      (replace-match "/"))))
+(require 'cl-lib)
 
 (defun resize-window ()
   "Resize the current window."
@@ -69,8 +24,8 @@
   (cond ((/= (count-windows) 2)
          (message "You need exactly 2 windows to do this."))
         (t
-         (let* ((w1 (first (window-list)))
-                (w2 (second (window-list)))
+         (let* ((w1 (cl-first (window-list)))
+                (w2 (cl-second (window-list)))
                 (b1 (window-buffer w1))
                 (b2 (window-buffer w2))
                 (s1 (window-start w1))
