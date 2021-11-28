7f6f20a2cec98a41301e4fab2d9803e9ac609552
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2021-11-28 15:46:17 +0900

    add switch buffer rule

--- emacs/switch-buffer-rules.el
+++ emacs/switch-buffer-rules.el
@@ -13,3 +13,11 @@
 (rule-based-switch-buffer-define view-component-erb
   :matcher (lambda (fn) (string-match "_component.html.erb$" fn))
   :mappers (lambda (fn) (replace-regexp-in-string "\\.html.erb$" ".rb" fn)))
+
+(rule-based-switch-buffer-define app-rb-to-spec-rb
+  :matcher (lambda (fn) (string-match "/app/.*.rb$" fn))
+  :mappers (lambda (fn) (replace-regexp-in-string "\\(.*\\)/app/\\(.*\\).rb$" "\\1/spec/\\2_spec.rb" fn)))
+
+(rule-based-switch-buffer-define spec-rb-to-app-rb
+  :matcher (lambda (fn) (string-match "/spec/.*_spec.rb$" fn))
+  :mappers (lambda (fn) (replace-regexp-in-string "\\(.*\\)/spec/\\(.*\\)_spec.rb$" "\\1/app/\\2.rb" fn)))
